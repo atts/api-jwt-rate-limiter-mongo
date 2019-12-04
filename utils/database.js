24867e163d28238a03f5980df4d117f7bbad16fc
Atul Mishra <atul.mishra0590@gmail.com>
2019-12-04 19:45:39 +0530

    changed mongo env variable

--- utils/database.js
+++ utils/database.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGO_LOCAL_CONN_URL;
-const connectOptions = { 
-  keepAlive: true, 
-  //reconnectTries: Number.MAX_VALUE,
-  useUnifiedTopology: true ,
-  useNewUrlParser: true
-}; 
+const mongoURI = process.env.MONGODB_URI;
+const connectOptions = {
+    useNewUrlParser: true
+};
 //Connect to MongoDB 
-mongoose.Promise = global.Promise; 
-mongoose.connect(mongoURI, connectOptions, (err, db) => 
-{ 
-  if (err) console.log(`Error`, err); 
-  console.log(`Connected to MongoDB`); 
-}); 
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, connectOptions, (err, db) => {
+    if (err) console.log(`Error`, err);
+    console.log(`Connected to MongoDB`);
+});
