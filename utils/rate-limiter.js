92d5542708d88a3223d4c78b2295e32ed683e834
Atul Mishra <atul.mishra0590@gmail.com>
2019-12-04 20:21:52 +0530

    redis heroku fix

--- utils/rate-limiter.js
+++ utils/rate-limiter.js
@@ -1,7 +1,6 @@
 var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDIS_URL);
-console.log("the redis url is " + redisURL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, { no_ready_check: true });
 client.auth(redisURL.auth.split(":")[1]);
 const RateLimit = require('express-rate-limit');
@@ -9,7 +8,8 @@ const RedisStore = require('rate-limit-redis');
 
 const limiter = new RateLimit({
     store: new RedisStore({
-        expiry: 60
+        expiry: 60,
+        client: redis.createClient({ url: redisURL, db: 2 })
     }),
     max: 100, // limit each IP to 100 requests. post limit error - "Too many requests, please try again later."
 });
