3fa2e85b3e31895e33a5fa42fe8253a8dd250e3e
Atul Mishra <atul.mishra0590@gmail.com>
2019-12-04 19:17:58 +0530

    added redis addon on heroku

--- utils/rate-limiter.js
+++ utils/rate-limiter.js
@@ -1,11 +1,16 @@
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDIS_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, { no_ready_check: true });
+client.auth(redisURL.auth.split(":")[1]);
 const RateLimit = require('express-rate-limit');
 const RedisStore = require('rate-limit-redis');
 
 const limiter = new RateLimit({
     store: new RedisStore({
-        expiry : 60
+        expiry: 60
     }),
     max: 100, // limit each IP to 100 requests. post limit error - "Too many requests, please try again later."
 });
 
-module.exports =  limiter 
+module.exports = limiter
