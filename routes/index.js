596f587795846893c14dc93ddcd58e2a4dbec814
Atul Mishra <atul.mishra0590@gmail.com>
2019-12-04 18:12:27 +0530

    added procfile for heroku

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res) {
-    res.render('connected', { title: 'Express' });
-});
-
-module.exports = router;
